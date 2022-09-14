30ed91607ce07363d535e0fe9141ca6ecdbe7a51
A7Aima <aimaabhilash@gmail.com>
2022-09-14 14:14:53 +0530

    push

--- src/app/source/remote-client.service.ts
+++ src/app/source/remote-client.service.ts
@@ -5,12 +5,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class RemoteClientService {
-  url = 'http://api.minebrat.com/api/v1';
+  url = 'https://api.minebrat.com/api/v1';
 
   constructor(private http: HttpClient) {}
 
   getStatesList() {
-    return this.http.get('/states');
+    return this.http.get(this.url + '/states');
   }
 
   getCitiesList(stateId: number) {

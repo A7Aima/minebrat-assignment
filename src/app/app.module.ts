b11b09084ec3f32ab867eebb076167b2c86bdba5
A7Aima <aimaabhilash@gmail.com>
2022-09-14 10:56:46 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { CitesListComponent } from './cites-list/cites-list.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CitesListComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/cites-list/cites-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cites-list',
+  templateUrl: './cites-list.component.html',
+  styleUrls: ['./cites-list.component.scss']
+})
+export class CitesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
